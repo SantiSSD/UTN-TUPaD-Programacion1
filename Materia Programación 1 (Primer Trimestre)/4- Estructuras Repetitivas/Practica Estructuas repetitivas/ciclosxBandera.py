e226e6fa155c0a700d542cbae7d666ff7b83509d
Santiago <santiago11gimenez@gmail.com>
2025-09-06 15:17:05 -0300

    eliminacion de carpeta sin uso

--- Materia Programación 1 (Primer Trimestre)/4- Estructuras Repetitivas/Practica Estructuas repetitivas/ciclosxBandera.py
+++
@@ -1,9 +0,0 @@
-
-for numero in range(1, 11):
-# primera vuelta del bucle
-    print("---------------------")
-    for multiplicar in range (1, 11):
-        tabla = numero * multiplicar
-        print(f"{numero} X {multiplicar} = {tabla}" )
-
-
--- Materia Programación 1 (Primer Trimestre)/4- Estructuras Repetitivas/Practica Estructuas repetitivas/sumatoria_promedio.py
+++
@@ -1,9 +0,0 @@
-cant_numeros = 5
-sumatoria = 0
-for cont in range(1, cant_numeros + 1):
-    print("Ingrese un numero", cont)
-    num = int(input())
-    sumatoria = sumatoria + num
-
-promedio = sumatoria / cant_numeros
-print(f"El promedio es {promedio} ")
